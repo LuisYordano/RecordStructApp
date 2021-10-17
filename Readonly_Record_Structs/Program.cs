e0b1c69072e21769f65e70d95fc63d28b367f63e
Luis Yordano <lycruzc@yahoo.com>
2021-10-17 05:46:09 -0500

    Add project files.

---
+++ Readonly_Record_Structs/Program.cs
@@ -0,0 +1,17 @@
+﻿Battery battery = new("CR2032", 0.235, 100);
+
+WriteLine(battery);
+
+while (battery.RemainingCapacityPercentage > 0)
+{
+    Battery updatedBattery = battery with { RemainingCapacityPercentage = battery.RemainingCapacityPercentage - 1 };
+    battery = updatedBattery;
+}
+
+WriteLine(battery);
+
+public readonly record struct Battery(string Model,
+                                      double TotalCapacityAmpHours,
+                                      int RemainingCapacityPercentage);
+
+
---
+++ Record_Structs/Program.cs
@@ -0,0 +1,16 @@
+﻿Battery battery = new("CR2032", 0.235, 100);
+
+WriteLine(battery);
+
+while (battery.RemainingCapacityPercentage > 0)
+{
+    battery.RemainingCapacityPercentage--;
+}
+
+WriteLine(battery);
+
+public record struct Battery(string Model, 
+                             double TotalCapacityAmpHours,
+                             int RemainingCapacityPercentage);
+
+
